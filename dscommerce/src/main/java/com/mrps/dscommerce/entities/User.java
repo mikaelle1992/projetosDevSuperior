7e73c290a26142fe0ce1faaf8c524b960fe8b8cb
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-06-28 19:22:52 -0300

    feature: classe payment

--- dscommerce/src/main/java/com/mrps/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/mrps/dscommerce/entities/User.java
@@ -42,7 +42,14 @@ public class User {
 		this.birthDate = birthDate;
 		this.password = password;
 	}
+	
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
